a025e589710818911e1a77aa1150a3ede6756311
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-13 18:37:53 +0100

    Add complete tabing functionality

--- src/index.js
+++ src/index.js
@@ -14,12 +14,20 @@ navList.addEventListener('click', (e) => {
     if (!e.target.dataset.tab) return;
     
     if (!e.target.classList.contains('nav-active')) {
-        Array.from(navItems).forEach(li => {
+        navItems.forEach(li => {
             li.classList.remove('nav-active')
         })
         e.target.classList.add('nav-active')
     }
 
     const targetTab = e.target.dataset.tab;
-    console.log(targetTab);
+
+    tabs.forEach(tab => {
+        // remove active class from all the elements in the array
+        tab.classList.add("hidden");
+        
+        if (tab.classList.contains(targetTab)) {
+            tab.classList.remove('hidden');
+        }
+    })
 })
