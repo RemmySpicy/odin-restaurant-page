a4cac668cf735afaec668a5b19ed7353a799b191
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-13 18:24:21 +0100

    Add javascript to activate and switch nav bar items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './style.css';
 
-console.log("Start");
+const navList = document.querySelector('.nav-list')
+const navItems = document.querySelectorAll('.nav-list li')
+const tabs = document.querySelectorAll(".tab")
+
+console.log(navItems)
+
+// tab.forEach(element => {
+    
+// });
+
+navList.addEventListener('click', (e) => {
+    if (!e.target.dataset.tab) return;
+    
+    if (!e.target.classList.contains('nav-active')) {
+        Array.from(navItems).forEach(li => {
+            li.classList.remove('nav-active')
+        })
+        e.target.classList.add('nav-active')
+    }
+
+    const targetTab = e.target.dataset.tab;
+    console.log(targetTab);
+})
