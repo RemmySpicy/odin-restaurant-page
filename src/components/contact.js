086c9021c034ec563be02ed1849ed8147d18d396
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-16 19:35:52 +0100

    Complete menu and contact styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,24 +5,27 @@ export default function createContact() {
     const markup = `
         <div class="container">
             <h2>Book A Table</h2>
+            <div class="container-for-contact">
+                <div class="contact-details">
+                    <p>Discover an exceptional dining journey at RemmySpicy's restaurant, where exquisite flavors 
+                        and impeccable service unite for a memorable experience. 
+                        Perfect for romantic evenings, family gatherings, and corporate events.
+                    </p>
+                </div>
+                <div class="reserve">
+                    <form action="">
+                        <label for="people-num">Number of People</label>
+                        <input type="number" min="1" max="15" name="people-num" id="" value="2">
+                        <label for="date">Choose Day</label>
+                        <input type="date">
+                        <label for="time">Select Time</label>
+                        <input type="time" name="" id="">
+                        <button class="btn-pry" type="submit"><a href="">Make Reservation</a></button>
+                    </form>        
+                </div>
+            </div>
+        </div>
 
-            <div class="contact-details">
-                <div class="about">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non rem debitis eius dolorem, mollitia, nemo sint, hic nihil fuga laudantium facere at.</p>
-            </div>
-            
-            <div class="reserve">
-                <form action="">
-                    <label for="people-num">Number of People</label>
-                    <input type="number" min="1" max="15" name="people-num" id="" value="2">
-                    <label for="date">Choose Day</label>
-                    <input type="date">
-                    <label for="time">Select Time</label>
-                    <input type="time" name="" id="">
-                    <button class="btn-pry" type="submit"><a href="">Make Reservation</a></button>
-                </form>
-            </div>
-        </div>    
     `;
 
     div.innerHTML = markup;
--- src/components/menu.js
+++ src/components/menu.js
@@ -19,19 +19,19 @@ export default function createMenu() {
                     <h3>Breakfast</h3>
                     <div class="food-1 img"></div>
                     <p>Medium bowl of Shrimpy Ricy Soupy thingy</p>
-                    <button><a href="">Order Now</a></button>
+                    <button class="btn-pry"><a href="">Order Now</a></button>
                 </li>
                 <li class="card">
                     <h3>Lunch</h3>
                     <div class="food-2 img"></div>
                     <p>Roasted rips with sliced tomatoes and potatoes</p>
-                    <button><a href="">Order Now</a></button>
+                    <button class="btn-pry"><a href="">Order Now</a></button>
                 </li>
                 <li class="card">
                 <h3>Dinner</h3>
                     <div class="food-3 img"></div>
-                    <p>Meaty vegetable dish on ceramic bown</p>
-                    <button><a href="">Order Now</a></button>
+                    <p>Meaty vegetable dish on medium size ceramic bowl</p>
+                    <button class="btn-pry"><a href="">Order Now</a></button>
                 </li>
             </ul>
         </div>
